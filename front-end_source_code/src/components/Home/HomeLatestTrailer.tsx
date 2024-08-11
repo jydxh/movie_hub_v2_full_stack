f3599234d609cae2ba42cc77d24b8f4fd1dddb94
jydxh <wzxxds@gmail.com>
2024-08-10 22:20:41 -0400

    done the error handling

--- front-end_source_code/src/components/Home/HomeLatestTrailer.tsx
+++ front-end_source_code/src/components/Home/HomeLatestTrailer.tsx
@@ -4,6 +4,7 @@ import { baseImgUrl } from "@/utils/types";
 import { useState } from "react";
 import backgroundColor from "@/utils/backgroundColor";
 import TrailerCard from "./TrailerCard";
+import Skeleton from "@mui/material/Skeleton";
 
 function HomeLatestTrailer() {
 	const { data, isError, isLoading } = useQuery({
@@ -15,8 +16,6 @@ function HomeLatestTrailer() {
 	const [imgCount, setImgCount] = useState(0);
 	const bgColor = backgroundColor(String(imgCount));
 
-	console.log(data);
-
 	return (
 		<section className="max-w-[1400px] mx-auto p-8 relative">
 			{data && (
@@ -25,14 +24,14 @@ function HomeLatestTrailer() {
 						style={{
 							backgroundImage: `url("${baseImgUrl}/w1920_and_h427_multi_faces/${data[imgCount].poster_path}")`,
 						}}
-						className=" flex justify-start items-center custom-scrollbar gap-x-6 object-fill absolute top-0 left-0 w-full h-full"></div>
+						className="  object-contain absolute top-0 left-0 w-full h-full"></div>
 					<div
 						className={`${bgColor} opacity-80 w-full h-full  top-0 left-0 absolute`}></div>
 					<div className="relative h-[16rem]">
 						<h4 className="capitalize font-bold text-xl text-white">
 							Latest Trailers
 						</h4>
-						<div className="flex overflow-x-scroll gap-x-4 ">
+						<div className="flex overflow-x-scroll gap-x-4 custom-scrollbar ">
 							{data.map((item, index) => (
 								<TrailerCard
 									key={item.id}
@@ -44,6 +43,36 @@ function HomeLatestTrailer() {
 						</div>
 					</div>
 				</>
+			)}
+			{isLoading && (
+				<>
+					<div className="absolute top-0 left-0 w-full h-full"></div>
+					<div
+						className={`bg-teal-800/90 w-full h-full  top-0 left-0 absolute`}></div>
+					<div className="relative h-[16rem]">
+						<h4 className="capitalize font-bold text-xl text-white">
+							Latest Trailers
+						</h4>
+						<div className="flex overflow-x-scroll gap-x-4 custom-scrollbar ">
+							{Array.from(
+								{ length: 10 },
+								(_, index) => index + 1
+							).map<React.ReactNode>(item => (
+								<Skeleton
+									key={item}
+									width={300}
+									height={200}
+									className="flex-shrink-0"
+								/>
+							))}
+						</div>
+					</div>
+				</>
+			)}
+			{isError && (
+				<p className="text-center w-full my-8 mx-auto">
+					Cannot fetch data, please try again!
+				</p>
 			)}
 		</section>
 	);

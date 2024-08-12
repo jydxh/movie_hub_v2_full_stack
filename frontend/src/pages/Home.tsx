0cc97b677645f8b96fee4315967556e5c9e65204
jydxh <wzxxds@gmail.com>
2024-08-11 23:59:34 -0400

    home page lazy loading

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,10 +1,33 @@
 import HomeSearch from "@/components/Home/HomeSearch";
 import HeroPride from "@/components/Home/HeroPride";
 
-import HomeTrending from "@/components/Home/HomeTrending";
-import HomePopular from "@/components/Home/HomePopular";
-import HomeFreeWatch from "@/components/Home/HomeFreeWatch";
-import HomeLatestTrailer from "@/components/Home/HomeLatestTrailer";
+/* when using lazy-loading, 6048.38ms load front-page */
+/* when not using 6902.80ms */
+// import HomeTrending from "@/components/Home/HomeTrending";
+// import HomePopular from "@/components/Home/HomePopular";
+// import HomeFreeWatch from "@/components/Home/HomeFreeWatch";
+// import HomeLatestTrailer from "@/components/Home/HomeLatestTrailer";
+
+import Skeleton from "@mui/material/Skeleton";
+import { lazy, Suspense } from "react";
+
+const LoadingSkeleton = () => {
+	return (
+		<div className="mx-auto my-8 text-center flex-col justify-center items-center">
+			<p className="text-3xl font-semibold">loading...</p>
+			<div className="mx-auto w-full">
+				<Skeleton variant="rounded" width={400} height={60} />;
+			</div>
+		</div>
+	);
+};
+
+const HomeTrending = lazy(() => import("@/components/Home/HomeTrending"));
+const HomePopular = lazy(() => import("@/components/Home/HomePopular"));
+const HomeFreeWatch = lazy(() => import("@/components/Home/HomeFreeWatch"));
+const HomeLatestTrailer = lazy(
+	() => import("@/components/Home/HomeLatestTrailer")
+);
 
 function Home() {
 	return (
@@ -13,14 +36,25 @@ function Home() {
 				<HomeSearch />
 				<HeroPride />
 
-				<HomeTrending />
-				{/* latest trailers */}
+				{/* 	<HomeTrending />
+			
 				<HomeLatestTrailer />
-				{/* popular */}
+			
 				<HomePopular />
-
-				{/* free to watch */}
-				<HomeFreeWatch />
+			
+				<HomeFreeWatch /> */}
+				<Suspense fallback={<LoadingSkeleton />}>
+					<HomeTrending />
+				</Suspense>
+				<Suspense fallback={<LoadingSkeleton />}>
+					<HomeLatestTrailer />
+				</Suspense>
+				<Suspense fallback={<LoadingSkeleton />}>
+					<HomePopular />
+				</Suspense>
+				<Suspense fallback={<LoadingSkeleton />}>
+					<HomeFreeWatch />
+				</Suspense>
 			</div>
 		</>
 	);
